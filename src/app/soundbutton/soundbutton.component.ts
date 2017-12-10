e893df76b7dd9dbd7774a6bc3c0890e890c927c8
Adrien Dourthe <adrien.dourthe@gmail.com>
2017-12-10 01:07:18 +0100

    style

--- src/app/soundbutton/soundbutton.component.ts
+++ src/app/soundbutton/soundbutton.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SoundbuttonComponent implements OnInit {
 
   @Input() label: String;
+  @Input() key: String;
 
   constructor() { }
 
